843317f2033f6a4b00e5707d9606f7363b8e8966
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-03-15 23:15:26 +0530

    routes and Controllers added 🎱

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,24 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { supabaseConnection } from "./database";
+import { logger } from "hono/logger";
+import { jwt } from "hono/jwt";
+import type { JwtVariables } from "hono/jwt";
+// import other apps
+import userRoutes from "./routes/user.routes";
+import memberRoutes from "./routes/member.routes";
+import adminRoutes from "./routes/admin.routes";
+// main app
 
-const app = new Hono().basePath("api/v1");
+const app = new Hono().basePath("/api/v1");
+
+app.use(logger());
 
 app.use(
   "/*",
   cors({
     origin: "http://localhost:5173",
-    allowMethods: ["POST", "GET", "PUT", "DELETE"],
+    allowMethods: ["POST", "GET", "PATCH", "DELETE"],
     credentials: true,
   })
 );
@@ -19,4 +29,19 @@ supabaseConnection().then(() => {
   console.log(`⚙️ Server is running on 3000`);
 });
 
-export { app };
+// Base route
+app.get("/", async (c) => {
+  return c.text("System Urrra denge !!! 🚀");
+});
+
+// 404 Not Found route
+app.get("/notfound", (c) => {
+  return c.notFound();
+});
+
+// Connect other apps
+app.route("/user", userRoutes);
+app.route("/admin", adminRoutes);
+app.route("/member", memberRoutes);
+
+export default app;
--- server/src/routes/member.routes.ts
+++ server/src/routes/member.routes.ts
@@ -1,2 +1,63 @@
-// View Bill Receipts
-// View bill notification
+import { Hono } from "hono";
+import { supabase } from "../database";
+import { ApiError } from "../utils/ApiError";
+import { resStatus } from "../utils/responseStatus";
+
+const app = new Hono();
+
+// !View bill notification
+app.get("/notifications", async (c) => {
+  const { userId } = await c.req.json();
+  try {
+    if (!userId) {
+      return c.body("User not found", resStatus.NotFound);
+    }
+    if (!supabase) {
+      throw new ApiError(400, "Database connection failed");
+    }
+
+    const { data: notifications, error } = await supabase
+      .from("notifications")
+      .select("*")
+      .eq("member_id", userId);
+
+    if (error) {
+      console.error("Notifications collection error: ", error);
+      throw new ApiError(resStatus.Forbidden, "Error Throwing from user details at Data fetching");
+    }
+
+    c.status(200);
+    return c.json({ data: notifications });
+  } catch (error) {
+    console.error("Error From User Notifications: ", error);
+    return c.body("Server Error", resStatus.InternalServerError);
+  }
+});
+
+//! View Bill Receipts
+app.get("/bill", async (c) => {
+  const { userId } = await c.req.json();
+  try {
+    if (!userId) {
+      return c.body("User not found", resStatus.NotFound);
+    }
+    if (!supabase) {
+      throw new ApiError(400, "Database connection failed");
+    }
+
+    const { data: bills, error } = await supabase.from("bill").select("*").eq("member_id", userId);
+
+    if (error) {
+      console.error("Bills collection error: ", error);
+      throw new ApiError(resStatus.Forbidden, "Error Throwing from user details at Data fetching");
+    }
+
+    c.status(200);
+    return c.json({ data: bills });
+  } catch (error) {
+    console.error("Error From User Bills: ", error);
+    return c.body("Server Error", resStatus.InternalServerError);
+  }
+});
+
+export default app;
