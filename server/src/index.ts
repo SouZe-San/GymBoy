c0480a6f722945889a3e534c504a5d2a0c4c0019
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-06-18 18:35:32 +0530

    Deploy Success 😀 😎

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ app.use(logger());
 app.use(
   "/*",
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://gym-boy.vercel.app"],
     allowMethods: ["POST", "GET", "PATCH", "DELETE", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
     credentials: true,
